7f431e3e86412edd656f7f8cd362da00b581fbb2
jvetting <jvetting@iastate.edu>
2020-07-07 16:39:18 -0400

    Added a new feedback form that functions same as contact page but for sending ratings along with a message

--- js/contact_me.js
+++ js/contact_me.js
@@ -19,6 +19,8 @@ $(function() {
       var city = $("input#city").val();
       var region = $("select#region").val();
       var postal_code = $("input#postal-code").val();
+      var confirmation = $("input#confirmation").val();
+      var reply = $("select#reply").val();
       console.log(name);
       console.log(email);
       console.log(phone);
@@ -29,6 +31,8 @@ $(function() {
       console.log(postal_code);
       console.log(exists);
       console.log(type);
+      console.log(confirmation);
+      console.log(reply);
 
       //Ticket format: year-month-day/short_uid
       //uid format: First initial + last intial + random num b/w 0-9 ex:'JV'
@@ -36,7 +40,7 @@ $(function() {
       var date = new Date();
       var name_arr = name.split(" ");
       var uid = name_arr[0].substring(0,1)+name_arr[1].substring(0,1)+(Math.floor(Math.random() * (9 - 0 + 1)) + 0);
-      var ticket = date.getFullYear()+"-"+date.getMonth()+"-"+date.getDay()+"/"+uid;
+      var ticket = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDay()+"/"+uid;
       console.log(ticket);
 
       //var message = header + $("textarea#message").val();//$("textarea#message").val();
@@ -66,6 +70,7 @@ $(function() {
         url: "././mail/contact_me.php",
         type: "POST",
         data: {
+          form: "contact",
           name: name,
           phone: phone,
           email: email,
@@ -77,7 +82,8 @@ $(function() {
           exists: exists,
           type: type,
           message: message,
-          ticket: ticket
+          ticket: ticket,
+          reply: reply
         },
 
         cache: false,
@@ -119,9 +125,173 @@ $(function() {
     e.preventDefault();
     $(this).tab("show");
   });
+
+  /**-------------------------------------------------------------------------------------------------------------------
+   * Link contact.html to feedback.html
+   * (w.i.p.)
+   */
+  /*
+  $("#feedbackContact input,#contactForm textarea").jqBootstrapValidation({
+
+  });
+   */
+  /**--------------------------------------------------------------------------------------------------------------------
+  Feedback form script
+   */
+  $("#feedbackForm input,#feedbackForm textarea").jqBootstrapValidation({
+    preventSubmit: true,
+    submitError: function($form, event, errors) {
+      // additional error messages or events
+    },
+    submitSuccess: function($form, event) {
+      event.preventDefault(); // prevent default submit behaviour
+      // get values from FORM
+      var email = $("input#email").val();
+      var confirmation = $("input#confirmation").val();
+      console.log(email);
+      //----------------------------------------------------------------------------------------------------------------
+      var header = document.getElementById("feedback");
+      var btns = header.getElementsByClassName("btn btn-feedback");
+      console.log("CONF:btns length:"+btns.length);
+      console.log(i)
+      var active = document.getElementsByClassName("btn btn-feedback active");
+      console.log("active length:"+active.length);
+      var last_active = active[active.length-1].id;
+      var rating = last_active.charAt(last_active.length-1);
+
+      console.log(rating);
+      console.log(confirmation);
+
+      //var message = header + $("textarea#message").val();//$("textarea#message").val();
+      //----------------------------------------------------------------------------------------------------------------
+      var message = $("textarea#feedbackMessage").val();
+      console.log(message);
+      $this = $("#sendMessageButton");
+      $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
+      /*
+      $.ajax({
+        url: "././mail/contact_me.php",
+        type: "POST",
+        data: {
+          name: name,
+          phone: phone,
+          email: email,
+          message: message
+        },
+      */
+
+      $.ajax({
+        url: "././mail/contact_me.php",
+        type: "POST",
+        data: {
+          form: "feedback",
+          name: name,
+          email: email,
+          message: message,
+          rating: rating
+        },
+
+        cache: false,
+        success: function() {
+          // Success message
+          $('#success').html("<div class='alert alert-success'>");
+          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+              .append("</button>");
+          $('#success > .alert-success')
+              .append("<strong>Your message has been sent. </strong>");
+          $('#success > .alert-success')
+              .append('</div>');
+          //clear all fields
+          $('#contactForm').trigger("reset");
+        },
+        error: function() {
+          // Fail message
+          $('#success').html("<div class='alert alert-danger'>");
+          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+              .append("</button>");
+          $('#success > .alert-danger').append($("<strong>").text("Sorry, it seems that my mail server is not responding. Please try again later!"));
+          $('#success > .alert-danger').append('</div>');
+          //clear all fields
+          $('#contactForm').trigger("reset");
+        },
+        complete: function() {
+          setTimeout(function() {
+            $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
+          }, 1000);
+        }
+      });
+    },
+    filter: function() {
+      return $(this).is(":visible");
+    },
+  });
 });
 
 /*When clicking on Full hide fail/success boxes */
 $('#name').focus(function() {
   $('#success').html('');
 });
+
+/*
+$(document).ready(function() {
+
+  // Get click event, assign button to var, and get values from that var
+  $('#feedback button').on('click', function () {
+    var thisBtn = $(this);
+
+    thisBtn.addClass('active').siblings().removeClass('active');
+    var btnId = thisBtn.id;
+    //var btnValue = thisBtn.val();
+    console.log(btnId);
+
+    //$('#selectedVal').text(btnValue);
+
+
+    // You can use this to set default value
+    // It will fire above click event which will do the updates for you
+    //$('#aBtnGroup button[value="M"]').click();
+
+    // Add active class to the current button (highlight it)
+    var header = document.getElementById("feedback");
+    var btns = header.getElementsByClassName("btn btn-feedback");
+    console.log("btns length:"+btns.length);
+    for (var i = 0; i < btns.length; i++) {
+      console.log(i)
+      btns[i].addEventListener("click", function() {
+        var current=$(this);
+        console.log("current:"+current.id);
+        var inactive = $('#feedback button');
+        //document.getElementsByClassName("btn btn-feedback");
+        var active =
+            //document.getElementsByClassName("btn btn-feedback active");
+
+        //console.log("id:"+this.id);
+
+        console.log("inactive length:"+inactive.length);
+        console.log("active length:"+active.length);
+
+        var rating = this.id.charAt(this.id.length-1);
+
+        console.log("rating:"+rating);
+
+        for(var i = 0; i < inactive.length; i++) {
+
+          if (i < rating) {
+            inactive[i].className = "btn btn-feedback active";
+            $('#feedback button[i]').className="btn btn-feedback active";
+          } else {
+            inactive[i].className = "btn btn-feedback";
+            $('#feedback button[i]').className="btn btn-feedback";
+          }
+
+
+          console.log(i);
+          console.log(inactive[i].id + ":" +inactive[i].className);
+
+        }
+      });
+    }
+  })
+});
+
+ */
--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,5 +1,170 @@
 <?php
+if(empty($_POST['form'])){echo "Form";}
+if(empty($_POST['email'])){echo "Email";}
+if(!filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)){echo "Email !valid";}
+if(empty($_POST['message'])){echo "Message";}
+else
+{
+   /**
+    * Contact email
+    */
+   $form = strip_tags(htmlspecialchars($_POST['form']));
+   if($form == "contact"){
+      // Check for empty fields
+      if(empty($_POST['name'])){echo "Name";}
+      if(empty($_POST['phone'])){echo "Phone";}
+      //if(empty($_POST['email'])){echo "Email";}
+      //if(!filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)){echo "Email !valid";}
+      if(empty($_POST['address_line1'])){echo "Address line 1";}
+      if(empty($_POST['city'])){echo "City";}
+      if(empty($_POST['region'])){echo "Region";}
+      if(empty($_POST['postal_code'])){echo "Postal code";}
+      if(empty($_POST['type'])){echo "Type";}
+      if(empty($_POST['exists'])){echo "Exists";}
+      //if(empty($_POST['message'])){echo "Message";}
+      if(empty($_POST['ticket'])){echo "Ticket";}
+      if(empty($_POST['reply'])){echo "Reply";}
+
+      if(empty($_POST['form'])      ||
+          empty($_POST['name'])      ||
+          empty($_POST['phone'])     ||
+          empty($_POST['email'])     ||
+          empty($_POST['address_line1']) ||
+          empty($_POST['city']) ||
+          empty($_POST['region']) ||
+          empty($_POST['postal_code']) ||
+          empty($_POST['type']) ||
+          empty($_POST['exists']) ||
+          empty($_POST['message'])   ||
+          empty($_POST['ticket'])   ||
+          empty($_POST['reply'])   ||
+          !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+      {
+         echo "No arguments Provided!";
+         return false;
+      }
+
+      $name = strip_tags(htmlspecialchars($_POST['name']));
+      $email_address = strip_tags(htmlspecialchars($_POST['email']));
+      $phone = strip_tags(htmlspecialchars($_POST['phone']));
+      $address_line1 = strip_tags(htmlspecialchars($_POST['address_line1']));
+      $address_line2 = "";
+      if(!empty($_POST['address_line2'])){$address_line2 = strip_tags(htmlspecialchars($_POST['address_line2']));}
+      $city = strip_tags(htmlspecialchars($_POST['city']));
+      $region = strip_tags(htmlspecialchars($_POST['region']));
+      $postal_code = strip_tags(htmlspecialchars($_POST['postal_code']));
+      $type = strip_tags(htmlspecialchars($_POST['type']));
+      $exists = strip_tags(htmlspecialchars($_POST['exists']));
+      $message = strip_tags(htmlspecialchars($_POST['message']));
+      $ticket = strip_tags(htmlspecialchars($_POST['ticket']));
+       $reply = strip_tags(htmlspecialchars($_POST['reply']));
+
+// Create the email and send the message
+      $to = 'jvetting@iastate.edu'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+      $email_subject = "Online Appointment Request: #$ticket";
+      $email_body = "A client has requested a service.\n\n"."Here are the details:\n\n
+Name: $name\n
+Email: $email_address\n
+Phone: $phone\n
+Address line 1: $address_line1\n
+Address line 2: $address_line2\n
+City: $city\n
+Region: $region\n
+Zip code: $postal_code\n
+Service type: $type\n
+State of service: $exists\n
+Preferred contact method: $reply\n
+Message:\n$message";
+      $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+      $headers .= "Reply-To: $email_address";
+      mail($to,$email_subject,$email_body,$headers);
+
+
+      /**---------------------------------------------------------------------------------------------------------------
+       * Confirmation email sent to client
+       */
+      // Create and send a confirmation email to client
+      $to = $email_address; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+      $email_subject = "Calvert Exterminators: Online Appointment Request Confirmation";
+      $email_body = "Your service request has been sent! We will reply within 2 business days!\n\n"."Here are the details:\n\n
+Ticket#: $ticket\n
+Name: $name\n
+Email: $email_address\n
+Phone: $phone\n
+Address line 1: $address_line1\n
+Address line 2: $address_line2\n
+City: $city\n
+Region: $region\n
+Zip code: $postal_code\n
+Service type: $type\n
+State of service: $exists\n
+Message:\n$message";
+      $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+      $headers .= "Reply-To: jvetting@iastate.edu";
+      mail($to,$email_subject,$email_body,$headers);
+   }
+   /**
+    * else, form==feedback
+    */
+   else{
+      /**
+       * Feedback email
+       */
+      // Check for empty fields
+      //if(empty($_POST['name'])){echo "Name";}
+      //if(empty($_POST['email'])){echo "Email";}
+      //if(!filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)){echo "Email !valid";}
+      //if(empty($_POST['message'])){echo "Message";}
+      if(empty($_POST['rating'])){echo "Rating";}
+
+      if(empty($_POST['form'])      ||
+          //empty($_POST['name'])      ||
+          empty($_POST['email'])     ||
+          empty($_POST['message'])   ||
+          empty($_POST['rating'])   ||
+          !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+      {
+         echo "No arguments Provided!";
+         return false;
+      }
+
+      $name = strip_tags(htmlspecialchars($_POST['name']));
+      $email_address = strip_tags(htmlspecialchars($_POST['email']));
+      $message = strip_tags(htmlspecialchars($_POST['message']));
+      $rating = strip_tags(htmlspecialchars($_POST['rating']));
+
+// Create the email and send the message
+      $to = 'jvetting@iastate.edu'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+      $email_subject = "Website Feedback Form";
+      $email_body = "A user has left feedback on the website.\n\n"."Here are the details:\n\n
+Email: $email_address\n
+Rating: $rating stars\n
+Message:\n$message";
+      $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+      $headers .= "Reply-To: $email_address";
+      mail($to,$email_subject,$email_body,$headers);
+
+      /**------------------------------------------------------------------------------------------------------------
+       * Confirmation email sent to user
+       * Don't really need to send one though
+       */
+      // Create and send a confirmation email to client
+      /*
+      $to = $email_address; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+      $email_subject = "Calvert Exterminators: Online Feedback Confirmation";
+      $email_body = "Your feedback has been sent! We will reply within 2 business days!\n\n"."Here are the details:\n\n
+Name: $name\n
+Email: $email_address\n
+Rating: $rating\n
+Message:\n$message";
+      $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+      $headers .= "Reply-To: jvetting@iastate.edu";
+      mail($to,$email_subject,$email_body,$headers);
+      */
+   }
+}
 // Check for empty fields
+/*
 if(empty($_POST['name'])){echo "Name";}
 if(empty($_POST['phone'])){echo "Phone";}
 if(empty($_POST['email'])){echo "Email";}
@@ -60,9 +225,10 @@ Message:\n$message";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
-
+*/
 
 // Create and send a confirmation email to client
+/*
 $to = $email_address; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Calvert Exterminators: Online Appointment Request Confirmation";
 $email_body = "Your service request has been sent! We will reply within 2 business days!\n\n"."Here are the details:\n\n
@@ -82,4 +248,5 @@ $headers .= "Reply-To: jvetting@iastate.edu";
 mail($to,$email_subject,$email_body,$headers);
 
 return true;
+*/
 ?>
